d8a1bca6c4b2f031450e0fdebcc27b1761abd438
Rohit Taneja <rohit.or.taneja@gmail.com>
2017-03-14 19:26:15 +0530

    bumped version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='jsonstreamer',
-      version='1.3.6',
+      version='1.3.7',
       author='Kashif Razzaqui',
       author_email='kashif.razzaqui@gmail.com',
       url='https://github.com/kashifrazzaqui/json-streamer',
